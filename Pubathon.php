f9daf4889b78e2cd6607805991fc94552a334a0e
jspu19 <sgjurquh@liverpool.ac.uk>
2024-04-02 13:35:56 +0100

    Commit message

--- Pubathon.php
+++ Pubathon.php
@@ -3,3 +3,15 @@
 // second commit test
 
 // third commit test
+
+<!DOCTYPE html>
+<html lang='en-GB'>
+<head>
+<title>PHP 02A</title>
+</head>
+<body>
+<h1>Our first PHP script</h1>
+<p><b>Hello <your name><br>
+Hello World!</b></p>
+</body>
+</html>
