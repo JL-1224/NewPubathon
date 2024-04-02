6645067b25aa443af348b9132672f5082a421fb6
jspu19 <sgjurquh@liverpool.ac.uk>
2024-04-02 13:43:08 +0100

    Commit message

--- Pubathon.php
+++ Pubathon.php
@@ -7,11 +7,25 @@
 <!DOCTYPE html>
 <html lang='en-GB'>
 <head>
-<title>PHP 02A</title>
+<title>PHP 02C</title>
 </head>
 <body>
-<h1>Our first PHP script</h1>
-<p><b>Hello <your name><br>
-Hello World!</b></p>
+<h1>Types and Type Casting</h1>
+<?php
+error_reporting( E_ALL );
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+echo "<h2>Types and Type Casting</h2>\n";
+$mode = rand(1,4);
+if ($mode == 1)
+$randvar = rand();
+elseif ($mode == 2)
+$randvar = (string) rand();
+elseif ($mode == 3)
+$randvar = rand()/(rand()+1);
+else
+$randvar = (bool) $mode;
+echo "Random scalar: $randvar<br>\n";
+?>
 </body>
 </html>
