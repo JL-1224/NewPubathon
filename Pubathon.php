206b941d309bad796212eb3d47fc388054386fea
JL-1224 <j_linstead@yahoo.co.uk>
2024-04-09 15:57:48 +0100

    Added dropdown menu for user to select area of city

--- Pubathon.php
+++ Pubathon.php
@@ -2,11 +2,15 @@
 <html lang='en-GB'>
 
 <head>
-// head content here
+
+// html head content here
+
 </head>
 
 <body>
-//body content here
+
+// html body content here
+
 <?php
 //Connecting database to script
 $db_hostname = "studdb.csc.liv.ac.uk";
@@ -14,7 +18,6 @@ $db_database = "sgjlinst";
 $db_username = "sgjlinst";
 $db_password = "Liverpool123"; 
 $db_charset = "utf8mb4";
-
 $dsn = "mysql:host=$db_hostname;dbname=$db_database;charset=$db_charset";
 $opt = array(
   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
@@ -23,12 +26,40 @@ $opt = array(
 );
 
 try {
-  // php code here
+  $pdo = new PDO($dsn, $db_username, $db_password, $opt);
+  
+  // Check if user has selected an area
+  if (isset($_POST['selectedArea'])) {
+    $selectedArea = $_POST['selectedArea'];
+    
+    // rest of php code here
+    
+  } else {
+    selectArea($pdo); // called if user is yet to select area
+  }
 } catch (PDOException $e) {
   exit("PDO Error: " . $e->getMessage() . "<br>");
 }
 
-// functions here
+// Displays dropdown of areas pulled from database
+function selectArea($pdo) {
+  echo "<form action='Pubathon.php' method='post'>
+        <label>Select area:</label>
+        <select name='selectedArea' required=true>
+        <option value=''></option>";
+        
+  $stmt = $pdo->query("SELECT DISTINCT area FROM pubs");
+  
+  while ($row = $stmt->fetch()) {
+      echo "<option value='" . $row["area"] . "'>" . $row["area"] . "</option>";
+  }
+  
+  echo "</select>
+        <input type='submit' value='Next'>
+        </form>";
+}
+
+// rest of functions here
 
 ?>
 </body>
