4979bbe61160358db387eab73c8795625b5f6f52
JL-1224 <j_linstead@yahoo.co.uk>
2024-04-15 14:22:02 +0100

    Fixed table

--- Pubathon.php
+++ Pubathon.php
@@ -176,19 +176,10 @@ function noOfPlayers($selectedArea, $selectedRules, $selectedFancyDress, $select
 function generate($pdo, $selectedArea) {
   $stmtPubs = $pdo->prepare("SELECT * FROM pubs WHERE area = :selectedArea ORDER BY RAND() LIMIT 9");
   $stmtPubs->execute(['selectedArea' => $selectedArea]);
-  
-  $stmtFancyDress = $pdo->prepare("SELECT * FROM fancyDress ORDER BY RAND() LIMIT 1");
-  $stmtFancyDress->execute();
 
   echo "<h2>Randomly Selected Pubs in $selectedArea:</h2>";
-
-  // Previous code displayed in list format
-  //foreach($stmt as $row) {
-    //echo "<li>Pub " . $i  . ": " . $row["name"] . " - " . $row["address"] . "</li>";
-    //$i += 1;
-  //}
   
-  // Updated code now displays as table
+  // Display crawl/golf in table
   echo "<table border='1'>
         <tr>
         <th>Pub</th>
@@ -197,7 +188,7 @@ function generate($pdo, $selectedArea) {
         </tr>";
         
   $i = 1;
-  foreach($stmt as $row) {
+  foreach($stmtPubs as $row) {
         echo "<tr>
             <td>Pub " . $i  . "</td>
             <td>" . $row["name"] . "</td>
