d0023c80908ff251f0200592159b86a5313d7624
Infochi <harryleahy627@gmail.com>
2024-04-16 17:46:29 +0100

    h1.2

--- Pubathon.php
+++ Pubathon.php
@@ -2,7 +2,7 @@
 <html lang='en-GB'>
 
 <head>
-  <meta charset="UTF-8">
+  <meta charset="utf8mb4">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <h1>Pubathon</h1>
   <style>
