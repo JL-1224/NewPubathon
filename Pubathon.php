327a50883ec8356dd65da04c7c4028d7f6c230c1
Infochi <harryleahy627@gmail.com>
2024-04-16 17:44:55 +0100

    h1

--- Pubathon.php
+++ Pubathon.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Pubathon</title>
+  <h1>Pubathon</h1>
   <style>
     /* CSS for styling the title */
     h1 {
